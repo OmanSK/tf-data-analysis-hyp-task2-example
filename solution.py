d6b18e161a686f81fdefd6166c48a0375225df46
OmanSK <110732673+OmanSK@users.noreply.github.com>
2023-04-13 17:51:03 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.stats import ks_2samp
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 143893840 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     p_value = 0.07
